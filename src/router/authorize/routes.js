16a7c3f65afef9dcd1356fce49ec417ff090257a
yongtaek12 <laundryday085@gmail.com>
2024-08-12 09:37:51 +0900

    Re-add all files and commit

--- src/router/authorize/routes.js
+++
@@ -1,4 +0,0 @@
-export default [
-  { path: '/authorize/sign-up', name: 'SignUp', component: () => import(/* webpackChunkName: "authorize.sign-up" */ '../views/member/Signup.vue')  },
-  { path: '/authorize/sign-in', name: 'SignIn', component: () => import(/* webpackChunkName: "authorize.sign-in" */ '../views/member/Signin.vue')  },
-]
--- src/vuex/getters.js
+++
@@ -1,9 +0,0 @@
-// src/vuex/getters.js
-export default {
-    getUserId: state => state.userId,
-    getErrorState: state => state.errorState,
-    getIsAuth: state => state.isAuth,
-    loggedIn(state) {
-      return !!state.user
-    }
-  }
