67d6015eb82eb75baeba904c9c7c49f123f5daa5
yongtaek12 <laundryday085@gmail.com>
2024-08-13 17:16:10 +0900

    로그인 화면 꾸미기

--- src/views/member/Signin.vue
+++ src/views/member/Signin.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="sign-in-page">
-      <h3>로그인</h3>
+      <!-- <h3>로그인</h3> -->
       <signin-form @submit="onSubmit"/>
-      <p>회원이 아니신가요? <router-link :to="{ name: 'Signup' }">회원가입 하러가기</router-link></p>
+      <!-- <p>회원이 아니신가요? <router-link :to="{ name: 'Signup' }">회원가입 하러가기</router-link></p> -->
     </div>
     </template>
     
--- src/views/member/SigninForm.vue
+++ src/views/member/SigninForm.vue
@@ -1,88 +1,132 @@
 <template>
-    <form @submit.prevent="onLogin">
-      <fieldset>
-        <input v-model.trim="formData.loginId" required type="text" placeholder="아이디를 입력해주세요."/>
-        <input :type="passwordInputType" v-model.trim="formData.loginPass" required placeholder="비밀번호를 입력해주세요."/>
-        <button type="button" @click="togglePasswordVisible">{{passwordViewButtonText}}</button>
-        <button type="submit">로그인</button>
-      </fieldset>
-    </form>
-  </template>
-  <script>
-  import userModel from '@/models/userModel'
+  <div class="local-container">
+    <div :class="['demo', { 's--switched': switched }]">
+      <div class="demo__inner">
+        <div class="demo__forms">
+          <div class="demo__form">
+            <div class="demo__form-content">
+              <form @submit.prevent="onLogin" class="form">
+                <div class="form__heading">안녕하세요</div>
+                <label class="form__field">
+                  <span class="form__field-label">Email</span>
+                  <input v-model="formData.loginId" class="form__field-input" type="text" placeholder="아이디를 입력해주세요" />
+                </label>
+                <label class="form__field">
+                  <span class="form__field-label">Password</span>
+                  <input :type="passwordInputType" v-model="formData.loginPass" class="form__field-input" placeholder="비밀번호를 입력해주세요" />
+                </label>
+                <button type="submit" class="form__submit">로그인</button>
+              </form>
+            </div>
+          </div>
+          <div class="demo__form">
+            <div class="demo__form-content">
+              <form @submit.prevent="onRegister" class="form">
+                <div class="form__heading">Time to feel like home</div>
+                <label class="form__field">
+                  <span class="form__field-label">Name</span>
+                  <input v-model="formData.registerName" class="form__field-input" type="text" placeholder="이름을 입력해주세요" />
+                </label>
+                <label class="form__field">
+                  <span class="form__field-label">Email</span>
+                  <input v-model="formData.registerEmail" class="form__field-input" type="text" placeholder="이메일을 입력해주세요" />
+                </label>
+                <label class="form__field">
+                  <span class="form__field-label">Password</span>
+                  <input v-model="formData.registerPass" class="form__field-input" type="password" placeholder="비밀번호를 입력해주세요" />
+                </label>
+                <button type="submit" class="form__submit">회원가입</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="demo__switcher">
+          <div class="demo__switcher-inner">
+            <div class="demo__switcher-content">
+              <div class="demo__switcher-text">
+                <div>
+                  <h3>New here?</h3>
+                  <p>Sign up and discover great amount of new opportunities!</p>
+                </div>
+                <div>
+                  <h3>One of us?</h3>
+                  <p>If you already have an account, just sign in. We&apos;ve missed you!</p>
+                </div>
+              </div>
+              <button @click="toggleSwitched" class="demo__switcher-btn">
+                <span class="animated-border"></span>
+                <span class="demo__switcher-btn-inner">
+                  <span>회원가입</span>
+                  <span>로그인</span>
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
-  export default {
-    name: 'Signin',
-    data () {
-        return {
-          ui: {
-            passwordVisible: false
-          },
-          formData: {
-            loginId: '',
-            loginPass: ''
-          }
-        }
-      },
-    methods: {
-      submit () {
-        const { email, password } = this
-        this.$emit('submit', { email, password })
-      }
-    },
-    computed: {
-        passwordInputType () {
-          return this.ui.passwordVisible ? 'text' : 'password'    
-        },
-        passwordViewButtonText () {
-          return this.ui.passwordVisible ? '감추기' : '보이기'
-        }      
-      },  
-    methods: {
-      togglePasswordVisible () {
-        this.ui.passwordVisible=!this.ui.passwordVisible
-      },
-      onLogin () {
-        // 폼검증
-        if(this.formData.loginId === '') {
-          alert('아이디를 입력하세요');
-          return;
-        }
-        if(this.formData.loginPass === '') {
-          alert('비밀번호를 입력하세요');
-          return;
-        }
-        userModel.requestLogin({
-          loginId: this.formData.loginId,
-          loginPass: this.formData.loginPass
-        }).then(() => {        
-          // 사용자의 로그인 처리완료시 / 페이지로 이동합니다.        
-          this.$router.replace('/')
-        })
-      }
-    }
-  }
-  </script>
+<script>
+import userModel from '@/models/userModel';
 
-
-
-/**
 export default {
-  name: 'SigninForm',
-  data () {
+  data() {
     return {
-      email: '',
-      password: ''
+      switched: false,
+      formData: {
+        loginId: '',
+        loginPass: '',
+        registerName: '',
+        registerEmail: '',
+        registerPass: ''
+      },
+      ui: {
+        passwordVisible: false
+      }
+    };
+  },
+  computed: {
+    passwordInputType() {
+      return this.ui.passwordVisible ? 'text' : 'password';
     }
   },
   methods: {
-    submit () {
-      const { email, password } = this
-      this.$emit('submit', { email, password })
+    toggleSwitched() {
+      console.log("Toggle Switched Called");
+
+      this.switched = !this.switched;
+    },
+    togglePasswordVisible() {
+      this.ui.passwordVisible = !this.ui.passwordVisible;
+    },
+    onLogin() {
+      if (this.formData.loginId === '') {
+        alert('아이디를 입력하세요');
+        return;
+      }
+      if (this.formData.loginPass === '') {
+        alert('비밀번호를 입력하세요');
+        return;
+      }
+      userModel.requestLogin({
+        loginId: this.formData.loginId,
+        loginPass: this.formData.loginPass
+      }).then(() => {
+        this.$router.replace('/about');
+      });
+    },
+    onRegister() {
+      // 회원가입 로직을 여기에 추가하세요.
+      alert('회원가입 폼 제출');
     }
   }
-}
+};
+</script>
 
-
+<style lang="scss">
+  @import '@/plugins/login'; // 챗봇 스타일 임포트
+</style>
 
 
