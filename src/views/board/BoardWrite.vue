e27e09c57388617d41c850d314cc7a76c2a638f3
yongtaek12 <laundryday085@gmail.com>
2024-08-23 18:37:57 +0900

    ckeditor5 통합하여 에디터 구현하는법 기능 몇가지 안됨

--- src/views/board/BoardWrite.vue
+++ src/views/board/BoardWrite.vue
@@ -19,7 +19,7 @@
     </div>
     <div class="board-contents">
       <ckeditor
-        v-model="contents"
+        v-model="editorData"
         :editor="editor"
         :config="editorConfig"
         @ready="onEditorReady"
@@ -49,7 +49,51 @@
 </template>
 
 <script>
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import {
+  ClassicEditor,
+  Bold,
+  Essentials,
+  Italic,
+  Mention,
+  Paragraph,
+  Undo,
+  Image,
+  ImageToolbar,
+  ImageCaption,
+  ImageStyle,
+  ImageUpload,
+  Link,
+  Table,
+  TableToolbar,
+  List,
+  BlockQuote,
+  CodeBlock,
+  Clipboard,
+  Underline,
+  Strikethrough,
+  Heading,
+  Font,
+  MediaEmbed,
+  InlineEditor,
+  FindAndReplace,
+  PasteFromOffice,
+  ImageResize,
+  Alignment,
+  Indent,
+  IndentBlock,
+  Subscript,
+  Superscript,
+  RemoveFormat,
+  TodoList,
+  Base64UploadAdapter
+} from 'ckeditor5';
+import 'ckeditor5/ckeditor5.css';
+import coreTranslations from 'ckeditor5/translations/es.js';
+import premiumFeaturesTranslations from 'ckeditor5-premium-features/translations/es.js';
+import '@ckeditor/ckeditor5-build-classic/build/translations/ko';  // 한국어 번역 추가
+
+// import FindAndReplace from '@ckeditor/ckeditor5-find-and-replace/src/findandreplace'; // 플러그인 불러오기
+// import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
 
 export default {
   data() {
@@ -62,35 +106,136 @@ export default {
       created_at: '',
       editor: ClassicEditor,
       editorData: '<p>Hello from CKEditor 5 in Vue!</p>',
-      editorConfig: {}, // 기본 설정으로 빈 객체로 남겨 둡니다.
+      editorConfig: {
+        language: "ko",
+        plugins: [
+          Essentials,
+          Bold,
+          Italic,
+          Paragraph,
+          Mention,
+          Undo,
+          Image,
+          ImageToolbar,
+          ImageCaption,
+          ImageStyle,
+          ImageUpload,
+          FindAndReplace,
+          Link,
+          Table,
+          TableToolbar,
+          List,
+          BlockQuote,
+          CodeBlock,
+          Clipboard,
+          Underline,
+          Strikethrough,
+          Heading, // 추가된 플러그인
+          Font,
+          MediaEmbed,
+          PasteFromOffice,
+          ImageResize,
+          Alignment,
+          Indent,
+          IndentBlock,
+          InlineEditor,
+          Subscript,
+          Superscript,
+          RemoveFormat,
+          TodoList,
+          Base64UploadAdapter
+        ],
+        toolbar: [
+          'findAndReplace', 'selectAll', '|',
+          'heading', '|',
+          'bold', 'italic', 'strikethrough', 'underline', 'code', 'subscript', 'superscript', 'removeFormat', '|',
+          'bulletedList', 'numberedList', 'todoList', '|',
+          'outdent', 'indent', '|',
+          'undo', 'redo', '|',
+          'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', 'highlight', '|',
+          'alignment', '|',
+          'link', 'insertImage', 'blockQuote', 'insertTable', 'mediaEmbed', 'codeBlock', 'htmlEmbed', '|',
+          'specialCharacters', 'horizontalLine', 'pageBreak', '|',
+          'textPartLanguage', '|',
+          'sourceEditing'
+        ],
+        shouldNotGroupWhenFull: true,
+
+        heading: {
+          options: [
+            {
+              model: "paragraph",
+              view: {
+                name: 'p',
+                classes: 'ck-heading_paragraph',
+                styles: {
+                  'text-align': 'left',  // 왼쪽 정렬
+                  'color': '#000000'     // 글씨 색 검은색
+                }
+              },
+              title: "본문",
+              class: "ck-heading_paragraph"
+            },
+            {
+              model: "heading1",
+              view: "h1",
+              title: "헤더1",
+              class: "ck-heading_heading1",
+            },
+            {
+              model: "heading2",
+              view: "h2",
+              title: "헤더2",
+              class: "ck-heading_heading2",
+            },
+            {
+              model: "heading3",
+              view: "h3",
+              title: "헤더3",
+              class: "ck-heading_heading3",
+            },
+          ]
+        },
+        fontSize: {
+          options: [9, 11, 13, 15, 17, 20, 24, 28, 32, 54],
+        },
+        alignment: {
+          options: ["justify", "left", "center", "right"],
+        },
+        image: {
+          resizeUnit: "px",
+          toolbar: [
+            "imageStyle:alignLeft",
+            "imageStyle:full",
+            "imageStyle:alignRight",
+            "|",
+            "imageTextAlternative",
+          ],
+          styles: ["full", "alignLeft", "alignRight"]
+        },
+        table: {
+          contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells']
+        },
+        typing: {
+          transformations: {
+            remove: ["enDash", "emDash", "oneHalf", "oneThird", "twoThirds", "oneForth", "threeQuarters"],
+          },
+        },
+        translations: [coreTranslations, premiumFeaturesTranslations],
+        placeholder: "글을 입력해보세요!",
+      }
     };
   },
   mounted() {
-
     this.fnGetView();
   },
   methods: {
-    onEditorReady(editor) {
-      this.editorInstance = editor;  // editor 인스턴스를 저장
-      this.setEditorHeight();
-    },
-    setEditorHeight() {
-      if (this.editorInstance) {
-        const editorElement = this.editorInstance.ui.view.editable.element;
-        if (editorElement) {
-          editorElement.style.minHeight = '500px';
-        }
-      }
-    },
     onFocus() {
-      // this.setEditorHeight();  // 포커스 시에도 높이 유지
-
+      // 포커스 시의 작업을 여기에 추가할 수 있습니다.
     },
     onBlur() {
-      // this.setEditorHeight();  // 블러 시에도 높이 유지
-
+      // 포커스 잃음 시의 작업을 여기에 추가할 수 있습니다.
     },
-  
     fnGetView() {
       if (this.idx !== undefined) {
         this.$axios
@@ -163,11 +308,31 @@ export default {
   },
 };
 </script>
+
 <style>
 .ck-editor__editable {
   min-height: 700px !important;
-  font-family: Arial, sans-serif;
-  color: #000 !important;
+}
+.ck-content p.ck-heading_paragraph {
+  text-align: left !important;
+  color: #000000 !important;
+}
+.ck.ck-content ul,
+.ck.ck-content ul li {
+  list-style-type: inherit;
+}
 
+.ck.ck-content ul {
+  /* Default user agent stylesheet, you can change it to your needs. */
+  padding-left: 40px;
+}
+
+.ck.ck-content ol,
+.ck.ck-content ol li {
+  list-style-type: decimal;
+}
+.ck.ck-content ol {
+  /* Default user agent stylesheet, you can change it to your needs. */
+  padding-left: 40px;
 }
 </style>
