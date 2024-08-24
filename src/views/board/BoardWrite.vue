55526e78ada4ba98fe3ea8e93096b9499cc3af68
yongtaek12 <laundryday085@gmail.com>
2024-08-24 22:21:07 +0900

    default font color white 로 나오는 에러 해결

--- src/views/board/BoardWrite.vue
+++ src/views/board/BoardWrite.vue
@@ -5,7 +5,7 @@
 			type="text"
 			v-model="title"
 			class="w3-input w3-border"
-			placeholder="제목을 입력해주세요."
+			placeholder="제목을 입력해주세요.@"
 			style="resize: none; width: 20%;"
 		/>
 		<input
@@ -310,6 +310,7 @@ export default {
 				options: [10, 12, 14, 'default', 18, 20, 22],
 				supportAllValues: true
 			},
+			
 			heading: {
 				options: [
 					{
@@ -377,8 +378,8 @@ export default {
 					'resizeImage'
 				]
 			},
-			initialData: 
-				'<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n    You\'ve successfully created a CKEditor 5 project. This powerful text editor will enhance your application, enabling rich text editing\n    capabilities that are customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n    <li>\n        <strong>Integrate into your app</strong>: time to bring the editing into your application. Take the code you created and add to your\n        application.\n    </li>\n    <li>\n        <strong>Explore features:</strong> Experiment with different plugins and toolbar options to discover what works best for your needs.\n    </li>\n    <li>\n        <strong>Customize your editor:</strong> Tailor the editor\'s configuration to match your application\'s style and requirements. Or even\n        write your plugin!\n    </li>\n</ol>\n<p>\n    Keep experimenting, and don\'t hesitate to push the boundaries of what you can achieve with CKEditor 5. Your feedback is invaluable to us\n    as we strive to improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n    <li>📝 <a href="https://orders.ckeditor.com/trial/premium-features">Trial sign up</a>,</li>\n    <li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n    <li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n    <li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n    <li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n    See this text, but the editor is not starting up? Check the browser\'s console for clues and guidance. It may be related to an incorrect\n    license key if you use premium features or another feature-related requirement. If you cannot make it work, file a GitHub issue, and we\n    will help as soon as possible!\n</p>\n',
+			initialData: '<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n    You\'ve successfully created a CKEditor 5 project. This powerful text editor will enhance your application, enabling rich text editing\n    capabilities that are customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n    <li>\n        <strong>Integrate into your app</strong>: time to bring the editing into your application. Take the code you created and add to your\n        application.\n    </li>\n    <li>\n        <strong>Explore features:</strong> Experiment with different plugins and toolbar options to discover what works best for your needs.\n    </li>\n    <li>\n        <strong>Customize your editor:</strong> Tailor the editor\'s configuration to match your application\'s style and requirements. Or even\n        write your plugin!\n    </li>\n</ol>\n<p>\n    Keep experimenting, and don\'t hesitate to push the boundaries of what you can achieve with CKEditor 5. Your feedback is invaluable to us\n    as we strive to improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n    <li>📝 <a href="https://orders.ckeditor.com/trial/premium-features">Trial sign up</a>,</li>\n    <li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n    <li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n    <li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n    <li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n    See this text, but the editor is not starting up? Check the browser\'s console for clues and guidance. It may be related to an incorrect\n    license key if you use premium features or another feature-related requirement. If you cannot make it work, file a GitHub issue, and we\n    will help as soon as possible!\n</p>\n',
+
 			language: 'ko',
 			link: {
 				addTargetToExternalLinks: true,
@@ -413,7 +414,6 @@ export default {
 			menuBar: {
 				isVisible: true
 			},
-			placeholder: 'Type or paste your content here!',
 			table: {
 				contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
 			},
@@ -503,13 +503,24 @@ export default {
 </script>
 
 <style>
+/**textarea 크기 조절 */
 .ck-editor__editable {
-  min-height: 1000px !important;
+  min-height: 1200px !important;
   min-width: 1200px !important;
+  /* color: black !important; */
 
   /* min-width: 800; */
 }
-
+.ck-content {
+    color: black !important;
+}
+/** CKEditor의 p 태그(Paragraph)의 색상을 검정색으로 강제 설정 */
+.ck-content p {
+  color: black !important;
+}
+.ck-content h1 {
+  color: black !important;
+}
 /*Toolbar*/
 .ck-editor__top {
 	min-width: 1200px;
