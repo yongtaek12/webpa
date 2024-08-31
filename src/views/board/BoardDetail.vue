22a01911bd2c97ef70a7b2a755d92600cd874343
yongtaek12 <laundryday085@gmail.com>
2024-08-31 14:30:43 +0900

    boardDetail 상세보기 ckeditor 적용 <css 가editor 와 bootstrap(기본설정) 이 서로 다르기때문에>

--- src/views/board/BoardDetail.vue
+++ src/views/board/BoardDetail.vue
@@ -9,10 +9,18 @@
         </div>
       </div>
       <div class="board-contents">
-        <!-- 기존 코드: <span>{{ contents }}</span> -->
-        <!-- 변경 코드: HTML 내용을 렌더링 -->
-        <!-- <span>{{ contents }}</span> -->
-        <div v-html="contents"></div>
+        <div class="main-container">
+          <div class="editor-container editor-container_classic-editor editor-container_include-block-toolbar" ref="editorContainerElement">
+            <div class="editor-container__editor">
+              <div ref="editorElement">
+              <ckeditor v-if="isLayoutReady" v-model="contents" :editor="editor" :config="config":disabled="true"
+			        @ready="onEditorReady"
+			        />
+            </div>
+          </div>
+        </div>
+      </div>        
+        <!-- <div v-html="contents"></div> -->
 
       </div>
       <div class="common-buttons">
@@ -24,20 +32,204 @@
   </template>
   
   <script>
+  import {
+	ClassicEditor,
+	AccessibilityHelp,
+	Alignment,
+	Autoformat,
+	Autosave,
+	BalloonToolbar,
+	Base64UploadAdapter,
+	BlockQuote,
+	Essentials,
+	Heading,
+	Highlight,
+	HorizontalLine,
+	HtmlComment,
+	HtmlEmbed,
+	ImageBlock,
+	ImageCaption,
+	ImageInline,
+	ImageInsert,
+	ImageInsertViaUrl,
+	ImageResize,
+	ImageStyle,
+	ImageTextAlternative,
+	ImageToolbar,
+	ImageUpload,
+	Indent,
+	IndentBlock,
+	Italic,
+	Link,
+	LinkImage,
+	List,
+	ListProperties,
+	Markdown,
+	MediaEmbed,
+	Mention,
+	PageBreak,
+	Paragraph,
+	PasteFromMarkdownExperimental,
+	PasteFromOffice,
+	RemoveFormat,
+	SelectAll,
+	ShowBlocks,
+	SourceEditing,
+	SpecialCharacters,
+	SpecialCharactersArrows,
+	SpecialCharactersCurrency,
+	SpecialCharactersEssentials,
+	SpecialCharactersLatin,
+	SpecialCharactersMathematical,
+	SpecialCharactersText,
+	Strikethrough,
+	Subscript,
+	Superscript,
+	Table,
+	TableCaption,
+	TableCellProperties,
+	TableColumnResize,
+	TableProperties,
+	TableToolbar,
+	TextPartLanguage,
+	TextTransformation,
+	Title,
+	TodoList,
+	Underline,
+	Undo
+} from 'ckeditor5';
+import 'ckeditor5/ckeditor5.css';
+import translations from 'ckeditor5/translations/ko.js';
   export default {
     data() { //변수생성
       return {
         requestBody: this.$route.query,
         idx: this.$route.query.idx,
-  
         title: '',
         author: '',
         contents: '',
-        created_at: ''
+        created_at: '',
+        isLayoutReady: false,
+			  config: null, // CKEditor needs the DOM tree before calculating the configuration.
+			  editor: ClassicEditor     
       }
     },
     mounted() {
       this.fnGetView()
+      // CKEditor 초기화
+    this.config = {
+			toolbar: {
+				items: [
+				],
+				shouldNotGroupWhenFull: false
+			},
+			plugins: [
+				AccessibilityHelp,
+				Alignment,
+				Autoformat,
+				Autosave,
+				BalloonToolbar,
+				Base64UploadAdapter,
+				BlockQuote,
+				Essentials,
+				Heading,
+				Highlight,
+				HorizontalLine,
+				HtmlComment,
+				HtmlEmbed,
+				ImageBlock,
+				ImageCaption,
+				ImageInline,
+				ImageInsert,
+				ImageInsertViaUrl,
+				ImageResize,
+				ImageStyle,
+				ImageTextAlternative,
+				ImageToolbar,
+				ImageUpload,
+				Indent,
+				IndentBlock,
+				Italic,
+				Link,
+				LinkImage,
+				List,
+				ListProperties,
+				Markdown,
+				MediaEmbed,
+				Mention,
+				PageBreak,
+				Paragraph,
+				PasteFromMarkdownExperimental,
+				PasteFromOffice,
+				RemoveFormat,
+				SelectAll,
+				ShowBlocks,
+				SourceEditing,
+				SpecialCharacters,
+				SpecialCharactersArrows,
+				SpecialCharactersCurrency,
+				SpecialCharactersEssentials,
+				SpecialCharactersLatin,
+				SpecialCharactersMathematical,
+				SpecialCharactersText,
+				Strikethrough,
+				Subscript,
+				Superscript,
+				Table,
+				TableCaption,
+				TableCellProperties,
+				TableColumnResize,
+				TableProperties,
+				TableToolbar,
+				TextPartLanguage,
+				TextTransformation,
+				Title,
+				TodoList,
+				Underline,
+				Undo
+			],
+
+
+			language: 'ko',
+			link: {
+				addTargetToExternalLinks: true,
+				defaultProtocol: 'https://',
+				decorators: {
+					toggleDownloadable: {
+						mode: 'manual',
+						label: 'Downloadable',
+						attributes: {
+							download: 'file'
+						}
+					}
+				}
+			},
+			list: {
+				properties: {
+					styles: true,
+					startIndex: true,
+					reversed: true
+				}
+			},
+			mention: {
+				feeds: [
+					{
+						marker: '@',
+						feed: [
+							/* See: https://ckeditor.com/docs/ckeditor5/latest/features/mentions.html */
+						]
+					}
+				]
+			},
+			menuBar: {
+				isVisible: true
+			},
+			table: {
+				contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+			},
+			translations: [translations]
+		};
+    this.isLayoutReady = true;
     },
     methods: {
       fnGetView() {
@@ -77,7 +269,15 @@
             }).catch((err) => {
           console.log(err);
         })
-      }
+      },
+      onEditorReady(editor) {
+        this.editorInstance = editor;  // editor 인스턴스를 저장
+        const toolbarElement = editor.ui.view.toolbar.element;
+        toolbarElement.style.display = 'none';
+
+        // CKEditor를 읽기 전용 모드로 설정
+        // editor.isReadOnly = true;  // CKEditor를 읽기 전용 모드로 설정
+      },
     }
   }
   </script>
