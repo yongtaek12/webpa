53f236f7a46a2d4bbe8a4086523489819e148904
yongtaek12 <laundryday085@gmail.com>
2024-11-01 17:18:40 +0900

    admin style css 로 옮김 텝 에따라 db 연동 url 분기 처리 생성일자 안보임 todo

--- src/views/admin/Admin.vue
+++ src/views/admin/Admin.vue
@@ -33,6 +33,59 @@
           <button class="add-role-button">권한 등록</button>
         </div>
       </div>
+
+      <div v-if="activeTab === 1">
+        <div class="tbl_wrp">
+          <table class="table rounded-table">
+            <thead>
+            <tr class="table-header">
+              <th scope="col">계정</th>
+              <th scope="col">이름</th>
+              <th scope="col">핸드폰 번호</th>
+              <th scope="col">상태</th>
+              <th scope="col">권한</th>
+              <th scope="col">권한</th>
+              <th scope="col">등록일</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="role in roles" :key="role.name" class="role-row">
+              <td>{{ role.userId }}</td>
+              <td>{{ role.nickname }}</td>
+              <td>{{ role.phone }}</td>
+              <td>{{ role.status }}</td>
+              <td>{{ role.roleDesc }}</td>
+              <td>{{ role.roleName }}</td>
+              <td>{{ role.createAt }}</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+        <div v-if="activeTab === 2">
+          <div class="tbl_wrp">
+            <table class="table rounded-table">
+              <thead>
+              <tr class="table-header">
+                <th scope="col">리소스명</th>
+                <th scope="col">리소스타입</th>
+                <th scope="col">HttpMethod</th>
+                <th scope="col">순서</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr v-for="role in roles" :key="role.name" class="role-row">
+                <td>{{ role.resourceName }}</td>
+                <td>{{ role.resourceType }}</td>
+                <td>{{ role.httpMethod }}</td>
+                <td>{{ role.orderNum }}</td>
+              </tr>
+              </tbody>
+            </table>
+            <button class="add-role-button">리소스 등록</button>
+          </div>
+        </div>
     </div>
   </div>
 </template>
@@ -54,6 +107,8 @@ export default {
 
     const selectTab = (index) => {
       activeTab.value = index;
+      fetchRoles(); // 탭이 변경될 때 데이터를 다시 가져옵니다.
+
     };
     // 데이터 가져오기
 
@@ -61,7 +116,14 @@ export default {
 
     const fetchRoles = async () => {
       try {
-        const response = await axios.get(`${serverUrl}/admin/roles`);
+        let response;
+        if (activeTab.value === 0) {
+          response = await axios.get(`${serverUrl}/admin/roles`);
+        } else if (activeTab.value === 1) {
+          response = await axios.get(`${serverUrl}/admin/users`);
+        } else if (activeTab.value === 2) {
+          response = await axios.get(`${serverUrl}/admin/resources`);
+        }
         console.log("response : ", response.data)
         roles.value = response.data; // roles 데이터 설정
       } catch (error) {
@@ -84,87 +146,5 @@ export default {
 </script>
 
 <style>
-.nav-tabs {
-  width: 80%;
-  margin: 0 auto;
-  border-bottom: 2px solid #ddd;
-}
 
-.tabs {
-  display: flex;
-  list-style: none;
-  padding: 0;
-  margin: 0;
-  border-bottom: 2px solid #ddd;
-}
-
-.tabs li {
-  padding: 10px 20px;
-  cursor: pointer;
-  border: 1px solid transparent;
-  border-bottom: none;
-}
-
-.tabs li.active {
-  border-color: #ddd;
-  border-bottom: 2px solid white;
-  font-weight: bold;
-}
-
-.tab-content {
-  padding: 20px;
-  border: 1px solid #ddd;
-}
-
-.tbl_wrp {
-  margin-top: 20px;
-  background-color: #060C1C;
-  padding: 15px;
-  border-radius: 10px;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-}
-
-.table {
-  width: 100%;
-  border-collapse: separate;
-  border-spacing: 0;
-  color: white;
-}
-
-.table-header {
-  background-color: #a8d0db;
-  color: black;
-  text-align: left; /* 텍스트 왼쪽 정렬 */
-}
-
-.table-header th {
-  padding: 10px;
-}
-
-.role-row {
-  background-color: #060C1C;
-}
-
-.role-row td {
-  padding: 10px;
-  text-align: left;
-  border-top: 1px solid #333;
-}
-
-.add-role-button {
-  margin-top: 10px;
-  padding: 8px 16px;
-  background-color: #4CAF50;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  align-self: flex-end; /* 버튼을 오른쪽으로 정렬 */
-}
-
-.add-role-button:hover {
-  background-color: #45a049;
-}
 </style>
